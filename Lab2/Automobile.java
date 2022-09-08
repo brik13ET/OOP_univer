b73eef615db7b12f863174a6ef1e4bbc62ff5d2e
brik13ET <aleev.ibragim0909@gmail.com>
2022-09-09 01:16:25 +0400

    Initial commit

---
+++ Lab2/Automobile.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+
+public class Automobile{
+	String Manufacature;
+	Model models[]; //10
+
+	public void SetManufacture(String manuf)
+	{
+		String Manufacture = manuf;
+	}
+	
+	public String GetManufacture()
+	{
+		return Manufacature;
+	}
+	
+	public class Model
+	{
+		public String Title;
+		public int Cost;
+		
+		public Model(String title, int cost )
+		{
+			Title = title;
+			Cost = cost;
+		}
+	}
+	
+	public void SetModelTitle(int index, String Name)
+	{
+		models[index].Title = Name;
+	}
+
+	public void SetModelCost(int index, int cost)
+	{
+		models[index].Cost = cost;
+	}
+
+	public String GetModelTitle(int index)
+	{
+		return models[index].Title;
+	}
+
+	public int GetModelCost(int index)
+	{
+		return models[index].Cost;
+	}
+
+	public int[] GetModelsCost()
+	{
+		int[] ret = new int[models.length];
+		for (int i = 0; i < models.length; i++)
+		{
+			ret[i]= models[i].Cost;
+		}
+		return ret;
+	}
+
+	public void ChangeCostOfModel(String model, int cost)
+	{
+		boolean found = false;
+		int i = 0;
+		while(found != true && i < models.length)
+		{
+			if(models[i].Title == model)
+			{
+				found = true;
+				models[i].Cost = cost;
+			}
+
+		}
+	}
+
+	public void AddModel(String Name, int cost)
+	{
+		Model[] newM = Arrays.copyOf(models, models.length+1);
+		newM[models.length] = new Model(Name, cost);
+		models = newM;
+	}
+
+	public void DelModel(String Name)
+	{
+		Model[] newM = new Model[models.length-1];
+		int outI = 0;
+
+		boolean found = false;
+		int i = 0;
+		while(found != true && i < models.length)
+		{
+			if(models[i].Title == Name)
+			{
+				found = true;
+			}
+		}
+		if(found)
+		{
+			for (i = 0; i < newM.length; i++)
+			{
+				if (models[i].Title != Name)
+				{
+					System.arraycopy(models, i, newM, outI, 1);
+					outI++;
+				}
+			}
+			models = newM;
+		}
+	}
+}
---
+++ Lab2/Motocycle.java
@@ -0,0 +1,125 @@
+import java.time.Instant;
+import java.util.Date;
+import java.util.Arrays;
+
+public class Motocycle {
+
+    String Manufacture;
+    Date lastEdit;
+    Model models;
+
+    public void SetManufacture(String manuf)
+	{
+        lastEdit = Date.from(Instant.now());
+		String Manufacture = manuf;
+	}
+	
+	public String GetManufacture()
+	{
+		return Manufacture;
+	}
+	
+	public class Model
+	{
+		public String Title;
+		public int Cost;
+        Model next;
+        Model prev;
+		
+		public Model(String title, int cost)
+		{
+			Title = title;
+			Cost = cost;
+		}
+	}
+	
+	public void SetModelTitle(int index, String Name)
+	{
+        lastEdit = Date.from(Instant.now());
+		models[index].Title = Name;
+	}
+
+	public void SetModelCost(int index, int cost)
+	{
+        lastEdit = Date.from(Instant.now());
+		models[index].Cost = cost;
+	}
+
+	public String GetModelTitle(int index)
+	{
+		return models[index].Title;
+	}
+
+	public int GetModelCost(int index)
+	{
+        Model c = models;
+        int i = 0;
+        while (c != null && i != index) {
+            c = c.next;
+            i++;
+        }
+        if 
+	}
+
+	public int[] GetModelsCost()
+	{
+		int[] ret = new int[models.length];
+		for (int i = 0; i < models.length; i++)
+		{
+			ret[i]= models[i].Cost;
+		}
+		return ret;
+	}
+
+	public void ChangeCostOfModel(String model, int cost)
+	{
+        lastEdit = Date.from(Instant.now());
+		boolean found = false;
+		int i = 0;
+		while(found != true && i < models.length)
+		{
+			if(models[i].Title == model)
+			{
+				found = true;
+				models[i].Cost = cost;
+			}
+
+		}
+	}
+
+	public void AddModel(String Name, int cost)
+	{
+        lastEdit = Date.from(Instant.now());
+		Model[] newM = Arrays.copyOf(models, models.length+1);
+		newM[models.length] = new Model(Name, cost);
+		models = newM;
+	}
+
+	public void DelModel(String Name)
+	{
+		Model[] newM = new Model[models.length-1];
+		int outI = 0;
+
+		boolean found = false;
+		int i = 0;
+		while(found != true && i < models.length)
+		{
+			if(models[i].Title == Name)
+			{
+				found = true;
+			}
+		}
+		if(found)
+		{
+			for (i = 0; i < newM.length; i++)
+			{
+				if (models[i].Title != Name)
+				{
+					System.arraycopy(models, i, newM, outI, 1);
+					outI++;
+				}
+			}
+			models = newM;
+		}
+	}
+}
